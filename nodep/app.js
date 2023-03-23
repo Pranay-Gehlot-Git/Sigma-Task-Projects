807f01a8f29ae5e12ce0b67f4c685e84e3c4675b
Pranay Gehlot <pranay07200@gmail.com>
2023-03-23 11:00:15 +0530

    Add files via upload

---
+++ nodep/app.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+
+let app = express();
+app.disable("x-powered-by");
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set("view engine", "ejs");
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
